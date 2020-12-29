e8e8159b6884c792c32b3cc42aa06916e5253e42
Nguyễn Quyết Thắng <62535477+TieuLang@users.noreply.github.com>
2020-12-29 21:09:28 +0700

    Add files via upload

--- WFST.py
+++ WFST.py
@@ -28,8 +28,6 @@ for i in c:
 for i in range(len(dictt)):
     dictt[i][1]=-math.log(dictt[i][1]/sl_tu,2)
 
-s="a"
-
 #Luu tu dien
 dict={key:value for key, value in dictt}
 
@@ -57,7 +55,6 @@ def Dijkstra(inp):
     for i in range(len(c)-1):
         tam=c[i].split(' ')
         s.append([int(tam[0]),int(tam[1]),float(tam[4])])
-
     n=int(s[len(s)-1][1])+1 #so luong dinh trong do thi
 
     #Khoi tao mang de luu cac gia tri can thiet
@@ -72,16 +69,16 @@ def Dijkstra(inp):
         ke.append(list([])) #Mang dung de luu cac dinh ke voi dinh i
         dai.append(list([])) #Mang dung de luu do dai tu dinh i den dinh ke no
         kt.append(True) #Mang de kiem tra dinh i da duoc duyet chua trong thuat toan dijkstra
-        d.append(int(200000000)) #Mang de luu duong di ngan nhat tu dinh dau toi dinh i
-        tr.append(int(200000000)) #Mang de luu dinh ke truoc dinh i trong duong di ngan nhat tu dinh dau toi dinh i
+        d.append(float(20000000000)) #Mang de luu duong di ngan nhat tu dinh dau toi dinh i
+        tr.append(float(20000000000)) #Mang de luu dinh ke truoc dinh i trong duong di ngan nhat tu dinh dau toi dinh i
 
     #Xay dung danh sach ke
     for i in range(len(s)):
         #Luu lai cac dinh lien thong voi dinh i
         ke[s[i][0]].append(s[i][1]) #Luu dinh ke voi dinh s[i][0] la dinh s[i][1] vao mang
         dai[s[i][0]].append(s[i][2]) #Luu do dai tu dinh s[i][0] den dinh s[i][1] vao mang
-        ke[s[i][1]].append(s[i][0]) #tuong tu nhu tren
-        dai[s[i][1]].append(s[i][2])
+        #ke[s[i][1]].append(s[i][0]) #tuong tu nhu tren
+        #dai[s[i][1]].append(s[i][2])
 
     #Khoi tao gia tri ban dau cho thuat toan dijkstra
     dau=int(0) #Khoi tao dinh bat dau la dinh 0
@@ -146,8 +143,9 @@ def WFSTSegmentation(dictionary, sent):
     f = open("tmp.txt", "wt", encoding='utf-8')
     nwords = len(words)
     i = 0
+    dai_max=float(0);
     for i in range(nwords):
-        for j in range(8):
+        for j in range(nwords):
             if i + j >= nwords:
                 break
             word = words[i]
@@ -156,7 +154,14 @@ def WFSTSegmentation(dictionary, sent):
             weight = dict.get(word)
             if weight == None:
                 continue
+            if (weight>dai_max):
+                dai_max=weight
             f.write("{} {} {} {} {}\n".format(i, i + j + 1, word, word, weight))
+
+    dai_max=dai_max*nwords
+    for i in range(nwords):
+        word = words[i]
+        f.write("{} {} {} {} {}\n".format(i, i + 1, word, word, dai_max))
     f.close()
     return Dijkstra(sent)
 
@@ -181,12 +186,54 @@ def Tach_Tu():
             dem+=1
         f1.write(out+'\n')
     f1.close()
-    print(dem/len(st_raw))
-#Tach_Tu()
+
+    #test
+    f1=open("test.txt",'r',encoding='utf-8')
+    f2=open("test_kq.txt",'w',encoding='utf-8')
+    st_real=list(map(str,f1.read().split('\n')))
+    st_raw=list([])
+    for i in range(len(st_real)):
+        tmp=""
+        for j in range(len(st_real[i])):
+            if (st_real[i][j]=='_'):
+                tmp=tmp+" "
+            else:
+                tmp=tmp+st_real[i][j]
+        st_raw.append(tmp)
+    dem=1
+    sl_tu=1
+    tp=0
+    fp=0
+    fn=0
+    for s in range(len(st_raw)):
+        if (st_raw[s] == ""):
+            break
+        for i in range(len(st_raw[s])):
+            if (st_raw[s][i]==' '):
+                sl_tu+=1
+        out = WFSTSegmentation(dict, st_raw[s])
+        for i in range(len(out)):
+            if (out[i]=='_'):
+                if (st_real[s][i]=='_'):
+                    tp+=1
+                fp+=1
+            if (st_real[s][i]=='_'):
+                fn+=1
+            if (out[i]==' ' and st_real[s][i]==' ') or (out[i]=='_' and st_real[s][i]=='_'):
+                dem+=1
+        f2.write(out+'\n')
+    precision=float(tp/fp)
+    recal=float(tp/fn)
+    f1_score=float(2*precision*recal/(precision+recal))
+    print("Precision: "+str(precision))
+    print("Recall: "+str(recal))
+    print("F1-score cua tach tu: "+str(f1_score))
+
+Tach_Tu()
 
 def Gan_Nhan_Tu_Loai():
-    #Nhan=list(["N","V","A","P","M","D","R","E","C","I","O","Z","X","CH"])
-    Nhan=list(["UN","NN","VB","PRP"])
+    Nhan=list(["N","V","A","P","M","D","R","E","C","I","O","Z","X","CH"])
+    #Nhan=list(["UN","NN","VB","PRP"])
     matrixA=list([])
 
     for i in range(len(Nhan)+1):
@@ -196,16 +243,12 @@ def Gan_Nhan_Tu_Loai():
         matrixA.append(tmp)
 
     f=open("data_gan_nhan.txt",'r',encoding='utf-8')
-    f1=open("Kq_TachTu.txt",'r',encoding='utf-8')
-    data_gan_nhan=list(map(str,"con/UN mèo/NN trèo/VB cây/UN cau/NN\ncon/UN chuột/NN trèo/VB cây/UN cau/NN\ncon/UN chuột/NN hỏi/VB con/UN mèo/NN\ncon/UN trèo/VB là/VB con/UN nào/PRP".split('\n')))
-    test_gan_nhan = list(map(str, f1.read().split('\n')))
-
+    data_gan_nhan = list(map(str,f.read().split('\n')))
+    f.close()
     for i in range(len(data_gan_nhan)):
         data_gan_nhan[i]=list(map(str,data_gan_nhan[i].split()))
         for j in range(len(data_gan_nhan[i])):
             data_gan_nhan[i][j]=list(map(str,data_gan_nhan[i][j].split('/')))
-    for i in range(len(data_gan_nhan)):
-        print(data_gan_nhan[i])
 
     def inc(x,y):
         x1=int(-1)
@@ -219,7 +262,6 @@ def Gan_Nhan_Tu_Loai():
                 y1=i
                 break
         matrixA[x1+1][y1]+=1
-        print(x,y,x1,y1)
 
     for i in range(len(data_gan_nhan)):
 
@@ -234,9 +276,156 @@ def Gan_Nhan_Tu_Loai():
             matrixA[i][j]+=1
             tmp+=matrixA[i][j]
         matrixA[i][len(matrixA)-1]=tmp
-    print(Nhan)
-    for i in range(len(matrixA)):
-        print(matrixA[i])
-    
+        for j in range(len(matrixA)-1):
+            matrixA[i][j]=float(matrixA[i][j]/matrixA[i][len(matrixA)-1])
+
+   #Tinh Matrix B
+    danh_sach_tu=list([])
+    for i in range(len(data_gan_nhan)):
+        for j in range(len(data_gan_nhan[i])):
+            if not (data_gan_nhan[i][j][0] in danh_sach_tu):
+                danh_sach_tu.append(data_gan_nhan[i][j][0])
+    matrixB=list([])
+    for i in range(len(Nhan)):
+        tmp=list([])
+        for j in range(len(danh_sach_tu)+1):
+            tmp.append(int(0))
+        matrixB.append(tmp)
+    for i in range(len(data_gan_nhan)):
+        for j in range(len(data_gan_nhan[i])):
+            matrixB[Nhan.index(data_gan_nhan[i][j][1])][danh_sach_tu.index(data_gan_nhan[i][j][0])]+=1
+    for i in range(len(matrixB)):
+        for j in range(len(matrixB[i])-1):
+            matrixB[i][j]+=1
+            matrixB[i][len(matrixB[i])-1]+=matrixB[i][j]
+        for j in range(len(matrixB[i])-1):
+            matrixB[i][j]=float(matrixB[i][j]/matrixB[i][len(matrixB[i])-1])
+
+    f1 = open("test.txt", 'r', encoding='utf-8')
+    test_gan_nhan = list(map(str, f1.read().split('\n')))
+    f2=open("Kq_GanNhan.txt",'w',encoding='utf-8')
+    for s in test_gan_nhan:
+        s1=list(map(str,s.split()))
+        if s1==[]:
+            continue
+        vtb=list([])
+        for i in range(len(Nhan)):
+            vtb.append([])
+        x=float(0)
+        Nhan_kq=[]
+        for i in range(len(s1)):
+            Nhan_kq.append("")
+
+        for i in range(len(Nhan)):
+            if (danh_sach_tu.count(s1[0])==0):
+                if (Nhan[i]=="X"):
+                    vtb[i].append(float(1))
+                else:
+                    vtb[i].append(float(0))
+            else:
+                vtb[i].append(float(matrixB[i][danh_sach_tu.index(s1[0])]))
+            if (vtb[i][0]>x):
+                x=vtb[i][0]
+                Nhan_kq[0]=Nhan[i]
+
+        for j in range(1,len(s1)):
+            for i in range(len(Nhan)):
+                if (danh_sach_tu.count(s1[j]) == 0):
+                    if (Nhan[i] == "X"):
+                        vtb[i].append(float(1))
+                    else:
+                        vtb[i].append(float(0))
+                else:
+                    vtb[i].append(x*matrixB[i][danh_sach_tu.index(s1[j])])
+            x=float(0)
+            for i in range(len(Nhan)):
+                if (vtb[i][j]>x):
+                    x=vtb[i][j]
+                    Nhan_kq[j]=Nhan[i]
+        out=""
+        for i in range(len(s1)-1):
+            out=out+s1[i]+'/'+Nhan_kq[i]+' '
+        out=out+s1[len(s1)-1]+'/'+Nhan_kq[len(Nhan_kq)-1]+'\n'
+        f2.write(out)
+    f1.close()
+    f2.close()
+
+    f=open("kq_GanNhan.txt",'r',encoding='utf-8')
+    out_gan_nhan = list(map(str, f.read().split('\n')))
+    f.close()
+    for i in range(len(out_gan_nhan)):
+        out_gan_nhan[i] = list(map(str, out_gan_nhan[i].split()))
+        for j in range(len(out_gan_nhan[i])):
+            out_gan_nhan[i][j] = list(map(str, out_gan_nhan[i][j].split('/')))
+
+    H=0
+    T=0
+    H1=0
+    f3=open("test_gan_nhan.txt","r+",encoding="utf-8")
+    data_gan_nhan = list(map(str, f3.read().split('\n')))
+    f.close()
+    for i in range(len(data_gan_nhan)):
+        data_gan_nhan[i] = list(map(str, data_gan_nhan[i].split()))
+        for j in range(len(data_gan_nhan[i])):
+            data_gan_nhan[i][j] = list(map(str, data_gan_nhan[i][j].split('/')))
+    for i in range(len(data_gan_nhan)):
+        for j in range(len(data_gan_nhan[i])):
+            T+=1
+            if (out_gan_nhan[i][j][1] != 'X'):
+                H1 += 1
+            if (data_gan_nhan[i][j]==out_gan_nhan[i][j]):
+                H+=1
+
+    print(str(float(H)/float(T)))
+
+    f1 = open("test_kq.txt", 'r', encoding='utf-8')
+    test_gan_nhan = list(map(str, f1.read().split('\n')))
+    f2 = open("test_Kq_GanNhan.txt", 'w', encoding='utf-8')
+    for s in test_gan_nhan:
+        s1 = list(map(str, s.split()))
+        if s1 == []:
+            continue
+        vtb = list([])
+        for i in range(len(Nhan)):
+            vtb.append([])
+        x = float(0)
+        Nhan_kq = []
+        for i in range(len(s1)):
+            Nhan_kq.append("")
+
+        for i in range(len(Nhan)):
+            if (danh_sach_tu.count(s1[0]) == 0):
+                if (Nhan[i] == "X"):
+                    vtb[i].append(float(1))
+                else:
+                    vtb[i].append(float(0))
+            else:
+                vtb[i].append(float(matrixB[i][danh_sach_tu.index(s1[0])]))
+            if (vtb[i][0] > x):
+                x = vtb[i][0]
+                Nhan_kq[0] = Nhan[i]
+
+        for j in range(1, len(s1)):
+            for i in range(len(Nhan)):
+                if (danh_sach_tu.count(s1[j]) == 0):
+                    if (Nhan[i] == "X"):
+                        vtb[i].append(float(1))
+                    else:
+                        vtb[i].append(float(0))
+                else:
+                    vtb[i].append(x * matrixB[i][danh_sach_tu.index(s1[j])])
+            x = float(0)
+            for i in range(len(Nhan)):
+                if (vtb[i][j] > x):
+                    x = vtb[i][j]
+                    Nhan_kq[j] = Nhan[i]
+        out = ""
+        for i in range(len(s1) - 1):
+            out = out + s1[i] + '/' + Nhan_kq[i] + ' '
+        out = out + s1[len(s1) - 1] + '/' + Nhan_kq[len(Nhan_kq) - 1] + '\n'
+        f2.write(out)
+    f1.close()
+    f2.close()
+
 
 Gan_Nhan_Tu_Loai()
